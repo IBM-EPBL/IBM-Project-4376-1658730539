2851cbc3618a8b8e771e27590be5480b87c19b99
ManojRajSoti <113764495+ManojRajSoti@users.noreply.github.com>
2022-11-18 15:15:34 +0545

    Add files via upload

---
+++ IBM/Project Development Phase/Sprint 3/Customer Care Registry/app.py
@@ -0,0 +1,13 @@
+from registry import create_app
+
+if __name__ == "__main__":
+    app = create_app().run(host="0.0.0.0", port=5000)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contac
